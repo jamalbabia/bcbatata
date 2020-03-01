93cd7052d2667acca9123a35a83c75aa551a45e6
jamalbabia <47610404+jamalbabia@users.noreply.github.com>
2020-03-01 05:32:51 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1"
+var prefix = "3"
 var adminprefix = 'b'
 
 
@@ -23,7 +23,7 @@ client.on("message", message => {
 //bc online
 
 
-  var prefix = "1";
+  var prefix = "3";
 
   client.on("message", message => {
   
